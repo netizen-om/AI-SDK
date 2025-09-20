ad15bd5ecbbd43fea51e17a8e20c86a62a5d0d04
netizen-om <borisagarom@gmail.com>
2025-09-20 23:03:06 +0530

    completed auth.js's signIn, jwt and session Callbacks

--- auth.ts
+++ auth.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
+import { getUserById } from "./modules/auth/action";
+import { se } from "date-fns/locale";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
@@ -74,8 +76,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
       return true;
     },
-    async jwt() {},
-    async session() {},
+    async jwt({token}) {
+        if(!token.sub) return token;
+        
+        const existingUser = await getUserById(token.sub)
+        if(!existingUser) return token;
+
+        token.name = existingUser.name;
+        token.email = existingUser.email;
+        token.role = existingUser.role;
+
+        return token;
+    },
+    async session({session, token}) {
+        if(token.sub && session.user) {
+            session.user.id = token.sub
+        }
+
+        if(token.sib && session.user) {
+            session.user.role = token.role
+        }
+
+        return session
+    },
   },
   secret: process.env.AUTH_SECRET,
   adapter: PrismaAdapter(db),

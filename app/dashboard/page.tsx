28f32f61863fdcbb8964bac0f7a5c75ba1709cad
netizen-om <borisagarom@gmail.com>
2025-09-24 16:58:07 +0530

    just dashboard table remaining

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,15 @@
+import AddNewButton from '@/modules/dashboard/components/add-new'
+import AddRepo from '@/modules/dashboard/components/add-repo'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div className='flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 w-full'>
+        <AddNewButton />
+        <AddRepo />
+      </div>
+    </div>
   )
 }
 

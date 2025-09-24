aa623a82e315b1d5ba19e1d6237a026be4eefd4e
netizen-om <borisagarom@gmail.com>
2025-09-24 17:07:31 +0530

    completed full dashboard UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,34 @@
-import AddNewButton from '@/modules/dashboard/components/add-new'
-import AddRepo from '@/modules/dashboard/components/add-repo'
-import React from 'react'
+import { getAllPlaygoundForUser } from "@/modules/dashboard/actions";
+import AddNewButton from "@/modules/dashboard/components/add-new";
+import AddRepo from "@/modules/dashboard/components/add-repo";
+import EmptyState from "@/modules/dashboard/components/empty-state";
+import ProjectTable from "@/modules/dashboard/components/project-table";
+import React from "react";
 
-const Page = () => {
+const Page = async () => {
+  const playground = await getAllPlaygoundForUser();
+
   return (
-    <div className='flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10'>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 w-full'>
+    <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
         <AddNewButton />
         <AddRepo />
       </div>
-    </div>
-  )
-}
 
-export default Page
+      <div className="mt-10 flex flex-col justify-center items-center w-full">
+        {playground && playground.length === 0 ? (
+          <EmptyState />
+        ) : (
+          <ProjectTable
+            projects={playground || []}
+            onDeleteProject={() => {}}
+            onUpdateProject={() => {}}
+            onDuplicateProject={() => {}}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Page;

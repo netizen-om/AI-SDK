5a1d6d3cb69cfd4e7e530aa1edc74894ce1e8e2f
netizen-om <borisagarom@gmail.com>
2025-09-24 12:37:11 +0530

    implemented dashboard sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,15 +1,44 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { getAllPlaygoundForUser } from "@/modules/dashboard/actions";
+import { DashboardSidebar } from "@/modules/dashboard/components/dashboard-siderbar";
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  <SidebarProvider>
-    <div className="flex min-h-screen w-full overflow-x-hidden">
-        <main className="flex-1">
-            { children }
-        </main>
-    </div>
-  </SidebarProvider>;
+  const playgroundData = await getAllPlaygoundForUser();
+
+  // Store icon names (strings) instead of the components themselves
+  const technologyIconMap: Record<string, string> = {
+    REACT: "Zap",
+    NEXTJS: "Lightbulb",
+    EXPRESS: "Database",
+    VUE: "Compass",
+    HONO: "FlameIcon",
+    ANGULAR: "Terminal",
+  };
+
+  const formattedPlaygoundData = playgroundData?.map((item) => ({
+    id : item.id,
+    name : item.title,
+    //TODO : STAR
+    starred : false,
+    icon : technologyIconMap[item.template] || "Code2"
+  }))
+
+  return (
+    <>
+      <SidebarProvider>
+        <div className="flex min-h-screen w-full overflow-x-hidden">
+
+            {/* @ts-ignore */}
+            <DashboardSidebar intialPlaygoundData={formattedPlaygoundData} />
+
+          <main className="flex-1">{children}</main>
+        </div>
+      </SidebarProvider>
+      ;
+    </>
+  );
 }
